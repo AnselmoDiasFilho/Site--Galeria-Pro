fdff16b346af2aded8614a7452408f4975802f77
Anselmo Dias Filho <anselmodiasyahoocombr@gmail.com>
2021-06-06 22:04:45 -0300

    add part of JavaScript

--- Projeto/js/form.js
+++ Projeto/js/form.js
@@ -5,8 +5,34 @@ const fields = document.querySelectorAll(['input.required'])
 function customValidation(event) {
     const field = event.target
 
-    //Trocar mensagem de required
-    field.setCustomValidity("Esse campo está inválido platininhaa ")
+
+    //logica para verificar se existem erros
+    function verifyErrors() {   //pode haver um error aqui   
+        let foundError = false;
+
+       for(let error in field.validity){
+           // se não for customError
+           //então verificar se tem erro
+           FoundError = error
+           if (error = "customError" && field.validity[erro]) {  //substitui o sinal de igual por diferente
+        }
+
+       }
+
+       return foundError;
+    }
+
+    const error = verifyError()
+    console.log(" Error Exists", error)
+
+    if (error) {
+        //Trocar mensagem de required
+        field.setCustomValidity("Esse campo está inválido platininhaa ")
+    } else {
+        field.setCustomValidity("")
+    }
+
+    
 }
 
 for (field of fields){
